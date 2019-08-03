43fcd7fc86e38e76ecbcb981e3b5c2baf747577b
AhmedKhalil777 <progeng_ahmed_khalil@outlook.com>
2019-08-03 04:46:04 +0200

    Model Modification First

---
+++ Models/LoginViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TAVSS.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        public string FName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string Lname { get; set; }
+
+        [Required]
+        [DataType(DataType.Password), MinLength(8), MaxLength(100)]
+        public string Password { get; set; }
+    }
+}
---
+++ Models/StudentModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using TAVSS.Models.Attributes;
+
+namespace TAVSS.Models
+{
+    public class StudentModel
+    {
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string Lname { get; set; }
+
+        [Required]
+        [DataType(DataType.Password), MinLength(8), MaxLength(100)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        public string ConfirmPassWord { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime DOB { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        [StringRange(AllowableValues = new[] { "M", "F" }, ErrorMessage = "Gender must be either 'M' or 'F'.")]
+        public string Gender { get; set; }
+
+    }
+}
